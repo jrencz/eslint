eb52c28db9cfef64d73da68be51c312a3116c666
Jarek Rencz <jaroslaw@rencz.pl>
2016-08-19 12:23:55 +0200

    Review update: more test cases

--- tests/lib/rules/symbol-description.js
+++ tests/lib/rules/symbol-description.js
@@ -25,11 +25,39 @@ ruleTester.run("symbol-description", rule, {
             code: "var foo = \"foo\"; Symbol(foo);",
             env: {es6: true}
         },
+
+        // Ignore if it's shadowed.
+        {
+            code: "var Symbol = function () {}; Symbol();",
+            env: {es6: true}
+        },
+        {
+            code: "Symbol(); var Symbol = function () {};",
+            env: {es6: true}
+        },
+        {
+            code: "function bar() { var Symbol = function () {}; Symbol(); }",
+            env: {es6: true}
+        },
+
+        // Ignore if it's an argument.
+        {
+            code: "function bar(Symbol) { Symbol(); }",
+            env: {es6: true}
+        },
     ],
 
     invalid: [
         {
             code: "Symbol();",
+            errors: [{
+                message: "Expected Symbol to have a description.",
+                type: "CallExpression"
+            }],
+            env: {es6: true}
+        },
+        {
+            code: "Symbol(); Symbol = function () {};",
             errors: [{
                 message: "Expected Symbol to have a description.",
                 type: "CallExpression"
